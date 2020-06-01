531bbc5a7a479a6026c2f09677666c2b40a3fa19
Glen Worsley <glen.worsley@gmail.com>
2020-06-01 11:52:27 +1000

    initial commit

---
+++ src/main/java/com/glenworsley/techtestQ2/TechtestQ2Application.java
@@ -0,0 +1,13 @@
+package com.glenworsley.techtestQ2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtestQ2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtestQ2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/glenworsley/techtestQ2/TechtestQ2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glenworsley.techtestQ2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechtestQ2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
